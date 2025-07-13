586321f4e30205b7c1c09eca16935573227c0dd5
Dilip Kumar S <dilip1100@gmail.com>
2025-07-13 19:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def apply_filters():
     if salesperson != 'All':
         dashboard.filtered_df = dashboard.filtered_df[dashboard.filtered_df['Salesperson'] == salesperson]
     if car_make != 'All':
-        dashboard.filtered_df = dashboard.filtered_df[dashboard.filtered_df['Car Make'] == car_make']
+        dashboard.filtered_df = dashboard.filtered_df[dashboard.filtered_df['Car Make'] == car_make]
     if car_model != 'All':
         dashboard.filtered_df = dashboard.filtered_df[dashboard.filtered_df']['Car Model'] == car_model]
     if car_year != 'All':
